be2c637ad0b9dd4443840d3a4486876b3349d8ab
FedosEkb <f-baranov@mail.ru>
2021-04-27 23:17:06 +0600

    test

--- Inc/main.h
+++ Inc/main.h
@@ -13,7 +13,7 @@
 #include <stdint.h>
 #include "stm32f2xx.h"
 
-#define TEST_DEFINE_FOR_GIT                             101500
+#define TEST_DEFINE_FOR_GIT                             100500
 
 /********************************************************************************/
 /*                                                                            	*/
