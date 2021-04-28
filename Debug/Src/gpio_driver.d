a2ba1e2c1aa36b791c74bc4dbd20ff8f1c588934
FedosEkb <f-baranov@mail.ru>
2021-04-28 12:38:58 +0600

    another git ignore change

--- Debug/Src/gpio_driver.d
+++ Debug/Src/gpio_driver.d
@@ -1,27 +1,27 @@
 Src/gpio_driver.o: ../Src/gpio_driver.c ../Inc/gpio_driver.h \
- C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f2xx.h \
- C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f207xx.h \
- C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Include/core_cm3.h \
- C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Include/cmsis_version.h \
- C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Include/cmsis_compiler.h \
- C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Include/cmsis_gcc.h \
- C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Include/mpu_armv7.h \
- C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/system_stm32f2xx.h
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f2xx.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f207xx.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/core_cm3.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/system_stm32f2xx.h
 
 ../Inc/gpio_driver.h:
 
-C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f2xx.h:
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f2xx.h:
 
-C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f207xx.h:
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f207xx.h:
 
-C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Include/core_cm3.h:
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/core_cm3.h:
 
-C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_version.h:
 
-C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_compiler.h:
 
-C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_gcc.h:
 
-C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/mpu_armv7.h:
 
-C:/cubeIDEwork/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/system_stm32f2xx.h:
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/system_stm32f2xx.h:
