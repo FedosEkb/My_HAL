7eef1b9075e2d3e21073b970fd3e083ea35e83e9
FedosEkb <f-baranov@mail.ru>
2021-05-11 20:43:36 +0600

    start to write i2c

---
+++ Debug/Src/i2s_driver.d
@@ -0,0 +1,30 @@
+Src/i2s_driver.o: ../Src/i2s_driver.c ../Inc/i2c_driver.h \
+ ../Inc/gpio_driver.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f2xx.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f207xx.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/core_cm3.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/system_stm32f2xx.h
+
+../Inc/i2c_driver.h:
+
+../Inc/gpio_driver.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f2xx.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/stm32f207xx.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_version.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include/system_stm32f2xx.h:
--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -7,6 +7,7 @@
 C_SRCS += \
 ../Src/SPI_driver.c \
 ../Src/gpio_driver.c \
+../Src/i2s_driver.c \
 ../Src/main.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
@@ -14,6 +15,7 @@ C_SRCS += \
 OBJS += \
 ./Src/SPI_driver.o \
 ./Src/gpio_driver.o \
+./Src/i2s_driver.o \
 ./Src/main.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
@@ -21,6 +23,7 @@ OBJS += \
 C_DEPS += \
 ./Src/SPI_driver.d \
 ./Src/gpio_driver.d \
+./Src/i2s_driver.d \
 ./Src/main.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
@@ -31,6 +34,8 @@ Src/SPI_driver.o: ../Src/SPI_driver.c Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F207IGHx -DSTM32F2 -c -I../Inc -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include" -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include" -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/SPI_driver.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Src/gpio_driver.o: ../Src/gpio_driver.c Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F207IGHx -DSTM32F2 -c -I../Inc -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include" -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include" -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/gpio_driver.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Src/i2s_driver.o: ../Src/i2s_driver.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F207IGHx -DSTM32F2 -c -I../Inc -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include" -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include" -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/i2s_driver.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Src/main.o: ../Src/main.c Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F207IGHx -DSTM32F2 -c -I../Inc -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Include" -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Drivers/CMSIS/Device/ST/STM32F2xx/Include" -I"C:/Users/User/STM32CubeIDE/workspace_1.6.1/My_HAL/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Src/syscalls.o: ../Src/syscalls.c Src/subdir.mk
